405f02ce4f5cb43f336548b0c867b723b3438be2
Rafael Catarino <rafaelcatarino55@gmail.com>
2021-10-27 15:21:01 -0300

    adiciona exercicio do bloco 9 e do bloco 10

---
+++ 1.fundamentos/bloco-09-javajcript-e-testes-assíncronos/dia-01-javascript-assíncrono-e-callbacks/exercicio.07.js
@@ -0,0 +1,17 @@
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
+
+it('uppercase "test" igual a "TEST"', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  })
+}); 
+
