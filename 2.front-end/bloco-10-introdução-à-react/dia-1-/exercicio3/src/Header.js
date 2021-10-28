665ce792261f5c50a2568ad708beef11fdc04248
Rafael Catarino <rafaelcatarino55@gmail.com>
2021-10-28 20:12:01 -0300

    adiciona exercicio/10.2

---
+++ 2.front-end/bloco-10-introdução-à-react/dia-1-/exercicio3/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <div>
+      <h1 className='header'>Conteúdos de Front-End</h1>
+    </div>
+  }
+}
+
+export default Header
